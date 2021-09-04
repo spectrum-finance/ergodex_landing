f25ec01deb561332d02949cc6e01e218ffb6f281
yashablack <iamyashablack@gmail.com>
2021-09-04 09:11:00 +0300

    Revert "Merge pull request #6 from ergolabs/gatsby-migration"
    This reverts commit ecfd6d28a6f3dc1f09d5e2fbf89d173fbc43ae0a, reversing
    changes made to 1dd383fe43e614501813b8272a70c01e1a6146e6.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
-  #!/bin/bash
+#!/bin/bash
 
 echo "Start deploy"
 ssh -tq root@144.76.60.105 '/bin/bash -l -c "source ~/.nvm/nvm.sh; cd /var/www/landing; git pull origin master;"'
 echo "Deployed Successfully!"
 
 exit 0
+
