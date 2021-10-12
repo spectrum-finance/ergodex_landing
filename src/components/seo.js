10d6ebe2f088f954dc6fdf7549cea0c0d91bca74
yashablack <iamyashablack@gmail.com>
2021-10-12 12:38:20 +0300

    update seo component with twitter og

--- src/components/seo.js
+++ src/components/seo.js
@@ -16,11 +16,16 @@ const SEO = ({ description, title, path }) => {
 
       <title>{ergodexTitle}</title>
 
-      <meta property="og:image" content={OGimage} />
+      <meta property="og:image" content='https://adapulse.io/wp-content/uploads/2021/06/2021-06-27-21.13.19-800x395.jpg' />
       <meta property="og:title" content={ergodexTitle} />
       <meta property="og:description" content={description} />
       <meta property="og:type" content={'website'} />
       <meta property="og:url" content={ergodexUrl + path} />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content="@ErgoDEX" />
+      <meta name="twitter:site" content="@ErgoDEX" />
+      <meta property="og:image" content='https://adapulse.io/wp-content/uploads/2021/06/2021-06-27-21.13.19-800x395.jpg' />
     </Helmet>
   )
 }
