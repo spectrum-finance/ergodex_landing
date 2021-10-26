64e523ecf4d8c1159a64a289e96705227285a61a
deadit <stdeadit@gmail.com>
2021-10-26 12:08:08 +0300

    update admin

--- src/pages/updates.js
+++ src/pages/updates.js
@@ -7,7 +7,6 @@ const IndexPage = ({
     allMarkdownRemark: { edges },
   },
 }) => {
-    console.log(edges);
   const Posts = edges
     .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
     .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
