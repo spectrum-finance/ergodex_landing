6faa5beed960a0fb26823e03e77affa8156f2731
Denis Kovalev <deniskovalev4791@gmail.com>
2022-06-01 17:42:08 +0300

    clean up

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-  #!/bin/bash
-
-echo "Start deploy"
-ssh -tq root@144.76.60.105 '/bin/bash -l -c "source ~/.nvm/nvm.sh; cd /var/www/landing; git pull origin master; yarn; yarn build;"'
-echo "Deployed Successfully!"
-
-exit 0
